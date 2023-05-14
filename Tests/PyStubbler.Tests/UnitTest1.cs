defc7781216cc69abed58baf882dc044c27c9f78
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-14 11:18:34 +0200

    First attempt at generating a golden master

--- Tests/PyStubbler.Tests/UnitTest1.cs
+++ Tests/PyStubbler.Tests/UnitTest1.cs
@@ -5,7 +5,14 @@ namespace PyStubbler.Tests
         [Fact]
         public void Test1()
         {
+        }
 
+        static void Main(string[] args)
+        {
+            string targetAssemblyPath = Path.Join(Directory.GetCurrentDirectory(), "ExampleCSharpLibrary.dll");
+            string destPath = Path.Join(Directory.GetCurrentDirectory(), "GoldenMaster", "ExampleCSharpLibrary");
+
+            StubBuilder.BuildAssemblyStubs(targetAssemblyPath, destPath);
         }
     }
 }
