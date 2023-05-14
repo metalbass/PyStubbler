0a1630f8ca8a71ffa4a3032f9fdc0bd34db3e24d
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-14 13:28:56 +0200

    Implement CompareAgainstGoldenMaster

--- Tests/PyStubbler.Tests/GoldenMasterTests.cs
+++ Tests/PyStubbler.Tests/GoldenMasterTests.cs
@@ -2,17 +2,33 @@ using System.Runtime.CompilerServices;
 
 namespace PyStubbler.Tests
 {
+    /// <summary>
+    /// https://blog.thecodewhisperer.com/permalink/surviving-legacy-code-with-golden-master-and-sampling
+    /// </summary>
     public class GoldenMasterTests
     {
+        private static string goldenMasterDirectory = Path.Join(GetProjectDirectory(), "GoldenMaster");
+
         [Fact]
-        public void Test1()
+        public void CompareAgainstGoldenMaster()
         {
+            string testDataFolder = Path.Join(Directory.GetCurrentDirectory(), "TestData");
+
+            GenerateStubs(testDataFolder);
+
+            // TODO: since this only works with one file, replace with texttest?
+            string relativePath = Path.Join("ExampleCSharpLibrary", "ExampleCSharpLibrary", "__init__.pyi");
+
+            string expected = File.ReadAllText(Path.Join(goldenMasterDirectory, relativePath));
+            string actual = File.ReadAllText(Path.Join(testDataFolder, relativePath));
+
+            Assert.Equal(expected, actual);
         }
 
         /// <summary> Regenerates the golden master </summary>
         private static void Main(string[] args)
         {
-            GenerateStubs(Path.Join(GetProjectDirectory(), "GoldenMaster"));
+            GenerateStubs(goldenMasterDirectory);
         }
 
         private static void GenerateStubs(string destinationFolder)
