efc4e69af866866f4ebe7dec397663a6f8741f9d
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-14 12:03:39 +0200

    Rename UnitTest1 -> GoldenMasterTests

--- Tests/PyStubbler.Tests/GoldenMasterTests.cs
+++ Tests/PyStubbler.Tests/GoldenMasterTests.cs
@@ -2,7 +2,7 @@ using System.Runtime.CompilerServices;
 
 namespace PyStubbler.Tests
 {
-    public class UnitTest1
+    public class GoldenMasterTests
     {
         [Fact]
         public void Test1()
