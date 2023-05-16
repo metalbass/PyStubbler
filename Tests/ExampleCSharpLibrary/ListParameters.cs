cdf63ffbe861d8b3d4aa0eb310ca2a78b4bf2fa0
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-16 20:52:57 +0200

    Split Classes.cs into multiple files

---
+++ Tests/ExampleCSharpLibrary/ListParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleCSharpLibrary
+{
+    public class ListParameters
+    {
+        public ListParameters(List<string> param) { }
+        public void Method(List<string> param) { }
+        public List<string> ReturnList() { return new(); }
+    }
+}
---
+++ Tests/ExampleCSharpLibrary/SingleVsFloatParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleCSharpLibrary
+{
+    public class SingleVsFloatParameters
+    {
+        public SingleVsFloatParameters(float param) { }
+        public void Method(float param) { }
+        public float ReturnFloat() { return 0; }
+    }    
+}
