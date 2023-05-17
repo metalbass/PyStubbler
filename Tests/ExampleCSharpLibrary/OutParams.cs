f886193dd03a9b9769678ff470a0d890d4b38fea
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-17 20:09:26 +0200

    Cover out params

---
+++ Tests/ExampleCSharpLibrary/OutParams.cs
@@ -0,0 +1,15 @@
+﻿namespace ExampleCSharpLibrary
+{
+    public class OutParams
+    {
+        public void GetSomething(out int value) { value = 0; }
+        public void GetSomething(out string value) { value = ""; }
+        
+        public bool TryGetSomething(out object value) 
+        {
+            value = null;
+
+            return false;
+        }
+    }
+}
