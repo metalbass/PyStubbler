dcab66cd07c8321ea0227b51cab754f7748a6266
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-17 20:17:15 +0200

    Cover IEnumerable

---
+++ Tests/ExampleCSharpLibrary/Enumerables.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace ExampleCSharpLibrary
+{
+    public class Enumerables
+    {
+        public void Enumerable(IEnumerable enumerable) { }
+        public void EnumerableString(IEnumerable<string> stringEnumerable) { }
+        public void EnumerableGeneric<T>(IEnumerable<T> genericEnumerable) { }
+        
+        public IEnumerable ReturnEnumerable() { return null; }
+        public IEnumerable<string> ReturnEnumerableString() { return null; }
+        public IEnumerable<T> ReturnEnumerableGeneric<T>() { return null; }
+
+    }
+}
