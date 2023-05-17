68db3712f391f8a84d5122ee58f86ff36c1666c3
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-17 20:11:51 +0200

    Cover ref params

---
+++ Tests/ExampleCSharpLibrary/RefParams.cs
@@ -0,0 +1,15 @@
+﻿namespace ExampleCSharpLibrary
+{
+    public class RefParams
+    {
+        public void GetSomething(ref int value) { }
+        public void GetSomething(ref string value) { }
+
+        public bool TryGetSomething(ref object value)
+        {
+            value = null;
+
+            return false;
+        }
+    }
+}
