b779b7ad6debf61098c24e540787126dca4ecf1a
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-17 21:05:01 +0200

    Cover arrays

---
+++ Tests/ExampleCSharpLibrary/Arrays.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleCSharpLibrary
+{
+    public class Arrays
+    {
+        public string[] Property => new string[] { "" };
+        public void Method (string[] something) { }
+        public void Method (string[][] something) { }
+        public void Method (List<string[]> something) { }
+    }
+}
