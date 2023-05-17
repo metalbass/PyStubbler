ef7a658d295fc7bb89ce16e3f6222098d5d96efd
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-17 20:19:06 +0200

    Cover IList

--- Tests/ExampleCSharpLibrary/ListParameters.cs
+++ Tests/ExampleCSharpLibrary/ListParameters.cs
@@ -3,7 +3,10 @@
     public class ListParameters
     {
         public ListParameters(List<string> param) { }
+        public ListParameters(IList<string> param) { }
         public void Method(List<string> param) { }
+        public void Method(IList<string> param) { }
         public List<string> ReturnList() { return new(); }
+        public IList<string> ReturnIList() { return null; }
     }
 }
