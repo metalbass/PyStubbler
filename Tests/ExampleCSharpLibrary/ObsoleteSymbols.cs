c9e55d40c72aad9492af0fc32861048bec0af8dd
Xavier Arias Botargues <xavier1923@gmail.com>
2023-05-17 19:32:36 +0200

    Cover ObsoleteSymbols
    Found a bug. Obsolete properties are not handled properly

---
+++ Tests/ExampleCSharpLibrary/ObsoleteSymbols.cs
@@ -0,0 +1,16 @@
+﻿namespace ExampleCSharpLibrary
+{
+    public class ObsoleteSymbols
+    {
+        public string NonObsoleteProperty => "";
+        public void NonObsoleteMethod() { }
+        public static void NonObsoleteStaticMethod() { }
+        
+        [Obsolete]
+        public string ObsoleteProperty => "";
+        [Obsolete]
+        public void ObsoleteMethod() { }
+        [Obsolete]
+        public static void ObsoleteStaticMethod() { }
+    }
+}
